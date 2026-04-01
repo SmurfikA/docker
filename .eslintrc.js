6ed255751576e6c6ff2558a7e7c931677474e8a9
SmurfikA <o.kozachenko@e-u.edu.ua>
2026-04-01 20:02:58 +0300

    init nestjs project with db and redis connection

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  rules: {},
+};
