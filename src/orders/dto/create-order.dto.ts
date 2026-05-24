a089932440f686efafc4975fbf93078b501b580c
Sergey-T-ban <s.trunka@e-u.edu.ua>
2026-05-24 20:27:51 +0300

    add order DTOs with nested validation

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsArray,
+  ArrayMinSize,
+  ValidateNested,
+} from 'class-validator';
+
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+import { CreateOrderItemDto } from './create-order-item.dto';
+
+export class CreateOrderDto {
+  @ApiProperty({
+    type: [CreateOrderItemDto],
+    example: [
+      { productId: 1, quantity: 2 },
+      { productId: 5, quantity: 1 },
+    ],
+  })
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => CreateOrderItemDto)
+  items: CreateOrderItemDto[];
+}
